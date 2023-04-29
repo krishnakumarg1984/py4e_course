0356d52b0cdf9572e48df674fe532da185ffcea8
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2023-04-29 19:34:15 +0100

    adds a symlinking shell script

---
+++ link_dotfiles_from_previous_dir.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# echo "$PWD"
+dotfiles_in_prev_dir=$(find -L .. -maxdepth 1 -type f -name ".*")
+# echo $dotfiles_in_prev_dir
+
+for dotfile in $dotfiles_in_prev_dir; do
+  # echo $dotfile
+  if [[ $dotfile =~ .*".gitignore" ]] || [[ $dotfile =~ .*".prev" ]] || [[ $dotfile =~ .vim-arsync ]] || [[ $dotfile =~ .*"asynctasks" ]] || [[ $dotfile =~ "null-ls-root" ]] || [[ $dotfile =~ .exrc ]]; then
+    echo -n ""
+  else
+    # echo "$dotfile"
+    ln -s "$dotfile" . 2>/dev/null
+  fi
+done
+
+ln -s ../.vscode . 2>/dev/null
+ln -s ../clean_builds.sh . 2>/dev/null
+ln -s ../CPPLINT.cfg . 2>/dev/null
+ln -s ../setup.cfg . 2>/dev/null
+ln -s ../.ruff.toml . 2>/dev/null
